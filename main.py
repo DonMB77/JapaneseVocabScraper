5a0d2e99017717e935b6a3d864c321f316075cf7
DonMB77 <purposefuldrifter@gmail.com>
2025-08-18 20:07:35 +0200

    deletion of words using latin characters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import nagisa
-import sys
+import re
 
 def scrape_japanese_words(url):
     try:
@@ -17,6 +17,7 @@ def scrape_japanese_words(url):
         word_tag_array = [[word, tag] for word, tag in zip(words, tags)]
         strings_to_delete = ['補助記号','空白','助詞','助動詞']
         filtered_words = [element for element in word_tag_array if not any(e in element for e in strings_to_delete)]
+        filtered_words = delete_latin_words_from_list(filtered_words)
 
         print(filtered_words)
         return words
@@ -28,9 +29,18 @@ def scrape_japanese_words(url):
         print(f"An unexpected error occurred: {e}")
         return []
 
+def has_latin_characters(element):
+    return bool(re.search('[a-zA-Z]', element))
+
+def delete_latin_words_from_list(list):
+    return_list = []
+    for sublist in list:
+        new_sublist = [word for word in sublist if not has_latin_characters(word)]
+        return_list.append(new_sublist)
+    return return_list
+
 if __name__ == "__main__":
 
-    print(sys.path)
+    user_input_url = input("Please input an URL:  ")
 
-    url_to_scrape = "https://www3.nhk.or.jp/news/easy/ne2025081511586/ne2025081511586.html"
-    japanese_words = scrape_japanese_words(url_to_scrape)
+    japanese_words = scrape_japanese_words(user_input_url)
