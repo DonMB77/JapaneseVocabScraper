7d0126c35374b86e9c260a05f0bae3b2d93af9a5
DonMB77 <purposefuldrifter@gmail.com>
2025-08-20 21:11:41 +0200

    main data processing code to util package

--- util/data_proccessing_unit.py
+++ util/data_proccessing_unit.py
@@ -1,4 +1,8 @@
 import re
+import requests
+from bs4 import BeautifulSoup
+import nagisa
+import util.data_proccessing_unit as dpu
 
 def has_latin_characters(element):
     return bool(re.search('[a-zA-Z0-9]', element))
@@ -9,3 +13,35 @@ def delete_latin_words_from_list(list):
         new_sublist = [word for word in sublist if not has_latin_characters(word)]
         return_list.append(new_sublist)
     return return_list
+
+def scrape_japanese_words(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        text_content = soup.get_text()
+
+        words = nagisa.tagging(text_content).words
+        tags = nagisa.tagging(text_content).postags
+        word_tag_array = [[word, tag] for word, tag in zip(words, tags)]
+        strings_to_delete = ['補助記号','空白','助詞','助動詞']
+        filtered_words = [element for element in word_tag_array if not any(e in element for e in strings_to_delete)]
+        filtered_words = dpu.delete_latin_words_from_list(filtered_words)
+
+        print(filtered_words)
+        return words
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error during request: {e}")
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return []
+
+if __name__ == "__main__":
+
+    user_input_url = input("Please input an URL:  ")
+
+    japanese_words = scrape_japanese_words(user_input_url)
