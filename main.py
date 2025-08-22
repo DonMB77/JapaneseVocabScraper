4a7bc996e79b0d82216c3487603359664a225450
DonMB77 <purposefuldrifter@gmail.com>
2025-08-22 22:27:31 +0200

    input field and first table

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from util import data_proccessing_unit
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ db = SQLAlchemy(app)
 
 class Vocab(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
+    readingJapanese = db.Column(db.String(50), nullable=False)
+    wordType = db.Column(db.String(6), nullable=False)
+    translation = db.Column(db.String(100), nullable=False)
     def __repr__(self) -> str:
         return f"Task {self.id}"
 
@@ -19,6 +22,11 @@ class Vocab(db.Model):
 def index():
     if request.method == "GET":
         return render_template("homepage.html")
+    if request.method == "POST":
+        words = data_proccessing_unit.scrape_japanese_words(request.form['url'])
+        firstFive = words[:5]
+        print(firstFive)
+        return render_template("homepage.html", words=firstFive)
     
 if __name__ in "__main__":
     with app.app_context():
